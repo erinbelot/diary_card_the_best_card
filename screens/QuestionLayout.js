a8c6cb4b553f6683f360f4eba6ce033b80a63a8c
Erin <erinbelot@gmail.com>
2020-07-12 00:15:20 +1000

    Feat: Add QuestionLayout page

---
+++ screens/QuestionLayout.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { TouchableOpacity, StyleSheet, Text, Image, View, Styles } from 'react-native';
+import NextButton from '../components/NextButton';
+import ListOfRangeButtons from '../components/ListOfRangeButtons';
+
+export default function QuestionLayout( { question, handleSaveAndNext, pageAnswer, setPageAnswer } ) {
+  return (
+    <>
+      <View style={styles.welcomeContainer}>
+        <Image
+          source={
+            __DEV__
+              ? require("../assets/images/green-man.png")
+              : require("../assets/images/green-man.png")
+          }
+          style={styles.welcomeImage}
+        />
+      </View>
+      <Text style={styles.developmentModeText}>{question}</Text>
+      <ListOfRangeButtons questions={['yes', 'no']} pageAnswer={pageAnswer} setPageAnswer={setPageAnswer} />
+      <NextButton onPress={() => { handleSaveAndNext(question) }} />
+    </>
+  )
+};
+
+const styles = StyleSheet.create({
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 20,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+});
